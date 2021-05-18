e77a1b02fb904494d83d5fb948748fa2e452fe30
Shreshth Tuli <shreshthtuli@gmail.com>
2021-05-18 16:48:32 +0100

    Update rpi steps

--- debug/rpi_run.sh
+++ debug/rpi_run.sh
@@ -1,13 +1,10 @@
-sudo apt update
-sudo apt install build-essential
-sudo apt install libprotobuf-dev libprotobuf-c-dev protobuf-c-compiler protobuf-compiler python-protobuf
-sudo apt-get install libcap-dev
-sudo apt-get install libnl-3-dev libnl-genl-3-dev
-sudo apt install libnet1-dev libbsd-dev
-sudo apt-get install asciidoctor
+sudo apt -y update
+sudo apt install -y build-essential git libprotobuf-dev python3-pip libprotobuf-c-dev protobuf-c-compiler protobuf-compiler libcap-dev libnl-3-dev libnl-genl-3-dev libnet1-dev libbsd-dev libnl-route-3-dev libnfnetlink-dev pkg-config asciidoctor
 
-git clone  https://github.com/xemul/criu.git criu
+git clone  https://github.com/checkpoint-restore/criu.git criu
 cd criu
 make clean
 make
 make install
+
+sudo apt install -y linux-tools-5.11.0-1007-raspi linux-tools-raspi
